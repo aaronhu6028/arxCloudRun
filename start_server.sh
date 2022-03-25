8e29e39a933af825aebab7ba6657dbafbba74496
aaronhu <aaronhu6028@gmail.com>
2022-03-26 00:11:55 +0800

    stop_server

--- start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull
-chmod a+x *.sh
-sname=${HOSTNAME#*-} 
-sname=${sname^^}
-nohup dotnet arxServer.dll $sname &
-ps ax | grep dotnet
-tail -f nohup.out
--- stop_server.sh
+++ stop_server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-ps ax | grep dotnet
-cd $HOME/arxCloudRun
 pkill -SIGINT dotnet
-while :
-do
-    ps ax | grep dotnet
-    sleep 1s
-done
-
+ps ax | grep dotnet
+tail -f nohup.out
