f1bfa4378fbbc15162abdd0af9fa99187a33075b
aaronhu <aaronhu6028@gmail.com>
2022-04-03 01:35:25 +0800

    setup

---
+++ setup_arx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set timezone
+sudo timedatectl set-timezone Asia/Taipei
+
+# install .net
+wget -V || sudo apt install -y wget 
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-runtime-6.0
+  
+# create rc.local
+sname=${HOSTNAME#*-}
+sname=${sname^^}
+printf '#!/bin/sh -e
+(cd /home/aaronhu6028/arxCloudRun && nohup dotnet arxServer.dll %s >> nohup.$(date --iso).out &)
+exit 0' $sname | sudo tee /etc/rc.local > /dev/null
+sudo chmod a+x /etc/rc.local
+
+echo "SETUP is done. Please restart the server."
