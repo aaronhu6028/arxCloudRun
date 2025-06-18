811cc601c06540a0ba824956d3533236ed98e9ca
aaronhu <aaronhu6028@gmail.com>
2025-06-18 23:46:06 +0800

    setup

--- setup_arx.sh
+++ setup_arx.sh
@@ -3,25 +3,25 @@
 # set timezone
 sudo timedatectl set-timezone Asia/Taipei
 
-# install .net
-wget -V || sudo apt install -y wget 
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# install .net 8
+wget -V || sudo apt install -y wget
+wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
-  sudo apt-get install -y dotnet-runtime-6.0
+  sudo apt-get install -y dotnet-runtime-8.0
 
 # install python3 modules
 sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
-sudo apt-get install -y python3-pip && \
-    pip3 install websockets asyncio
+sudo apt-get install python3-websockets
 
 # unzip arx-server.zip
-unzip -v || sudo apt install -y unzip 
-unzip -P jack1234 -o arx-server.zip
+unzip -v || sudo apt install -y unzip
+unzip -P jack1234 -o brx-server.zip
+cp -f arx-server.json arx-server.json
 
 # swap
 SWAP=/swap
@@ -37,8 +37,6 @@ else
 fi
 
 # create rc.local
-sname=${HOSTNAME#*-}
-sname=${sname^^}
 echo "#!/bin/sh
 sudo -H -u ${LOGNAME} bash ${HOME}/arxCloudRun/rc_local.sh" | sudo tee /etc/rc.local > /dev/null
 
